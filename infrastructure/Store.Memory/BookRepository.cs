c7df2b30d7758028b0a016a5507f279a7397d9f6
golnoshchekov.m <golnoshchekov.m@tecomgroup.ru>
2021-05-07 10:48:42 +0300

    Add projects

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programing Language"),
+        };
+
+        public Book[] GetAllBytitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
